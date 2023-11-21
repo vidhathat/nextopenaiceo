daa1d47c4226a5af414e82a97f65a754251cfe24
Deveshb15 <deveshb583@gmail.com>
2023-11-22 01:41:32 +0530

    feat: generate image

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react';
-import * as htmlToImage from 'html-to-image';
+/* eslint-disable @next/next/no-img-element */
+import { useState } from "react";
+import * as htmlToImage from "html-to-image";
+import "../styles/global.css";
 
-const html = `
+const html = (name, imageUrl) => {
+  return `
 <body style="background-color: #000; padding: 0; margin: 0; font-family: 'PT Serif', serif;">
    <main style="width: 100%; height: 100vh; position: relative;">
      <img
-       src="https://tii.imgix.net/production/articles/11755/24192ed0-ab01-4ab9-b4d7-b6bf2bdd40fb.png?fm=jpeg&amp;auto=compress&amp;w=1600&amp;h=900"
+       src="${imageUrl}"
        alt="nextopenaiceo"
        id="main-image"
        style="position: absolute; width: 100%; height: 75vh;"
@@ -18,11 +21,11 @@ const html = `
          <p class="third-topic" style="margin-left: 20px; font-size: 0.85rem;font-weight: 100;  color: #fff;">AI</p>
          <p class="fourth-topic" style="margin-left: 20px; font-size: 0.85rem;font-weight: 100;  color: #fff;">Venture Capital</p>
        </div>
-       <h1 style="margin-bottom: 8px; margin-top:0; font-weight: 100;">Former GitHub CEO Friedman, Scale AI Founder Wang Declined OpenAI Top Job</h1>
+       <h1 style="margin-bottom: 8px; margin-top:0; font-weight: 100;">OpenAI's Board approached ${name} About CEO Role</h1>
        <div class="divider" style="opacity: 0.3; margin-top: 20px; margin-bottom: 20px; background-color: #9b9b9b; height: 2px;"></div>
-       <p class="main-description" style="font-size: 1.1rem; font-weight: 100; color: #fff; line-height: 2;">
+       <p class="main-description" style="font-size: 1.1rem; font-weight: 100; color: #fff; line-height: 2; margin-bottom: 10px;">
          Before Emmett Shear accepted the Interim CEO role at OpenAI, the board
-         offered it to Nat Friedman, the former CEO of Microsoft-owned GitHub
+         offered it to ${name}, the former CEO of Microsoft-owned GitHub
          and a prolific investor in artificial intelligence startups, as well
          as Alex Wang, co-founder and CEO of Scale AI, according to people
          familiar with the matter. Both declined the offer, the people said.
@@ -38,34 +41,100 @@ const html = `
    </main>
  </body>
 `;
+};
 
 export default function Home() {
- const [image, setImage] = useState(null);
+  const [image, setImage] = useState(null);
+  const [name, setName] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
 
- const generateImage = async () => {
-  const node = document.getElementById('html-node');
-  const dataUrl = await htmlToImage.toPng(node);
-  console.log(dataUrl);
-  setImage(dataUrl);
- };
+  const generateImage = async () => {
+    console.log(name, imageUrl);
+    if (name.length === 0 || imageUrl.length === 0) {
+      alert("Please enter a name and image url");
+      return;
+    } else {
+      const node = document.getElementById("html-node");
+      // make node display block so that html2canvas can detect it
+      node.style.display = "block";
+      const dataUrl = await htmlToImage.toPng(node);
+      // make node display none again
+      node.style.display = "none";
+      console.log(dataUrl);
+      setImage(dataUrl);
+    }
+  };
 
- const downloadImage = () => {
-  const link = document.createElement('a');
-  link.download = 'image.png';
-  link.href = image;
-  link.click();
- };
+  const downloadImage = () => {
+    const link = document.createElement("a");
+    link.download = "image.png";
+    link.href = image;
+    link.click();
+  };
 
- return (
-  <div>
-    <div id="html-node" dangerouslySetInnerHTML={{ __html: html }} />
-    <button onClick={generateImage}>Generate Image</button>
-    {image && (
-      <div>
-        <img src={image} alt="Generated" />
-        <button onClick={downloadImage}>Download Image</button>
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
+  return (
+    <div>
+      <div
+        id="html-node"
+        style={{ display: "none" }}
+        dangerouslySetInnerHTML={{
+          __html: html(capitalizeFirstLetter(name), imageUrl),
+        }}
+      />
+      <div className="flex flex-col items-center h-screen justify-center">
+        <p className="text-4xl mb-6 text-white">
+          Be the Next <span style={{ color: "#4185F4" }}>OpenAI</span> CEO
+        </p>
+        <img
+          className="mb-10 w-[600px] h-[500px]"
+          src={
+            image ||
+            "https://i.ibb.co/Ns48NDm/F-b-ZX9-Cbc-AA4-Wcw-format-jpg-name-large.jpg"
+          }
+          alt="User Image"
+        />
+        <div className="flex justify-center items-center mb-4">
+          <input
+            type="text"
+            id="name"
+            className="bg-gray-500 hover:bg-gray-700 text-white font-normal py-2 px-4 rounded mx-2 placeholder-white"
+            placeholder="Enter a Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="text"
+            id="image"
+            className="bg-gray-500 hover:bg-gray-700 text-white font-normal py-2 px-4 rounded mx-2 placeholder-white"
+            placeholder="Paste Image URL"
+            value={imageUrl}
+            onChange={(e) => setImageUrl(e.target.value)}
+          />
+        </div>
+        {
+            image ? (
+<button
+          onClick={downloadImage}
+          style={{ width: "468px" }}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-normal py-2 px-4 rounded mt-4"
+        >
+          Download image
+        </button>
+            ) : (
+                <button
+          onClick={generateImage}
+          style={{ width: "468px" }}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-normal py-2 px-4 rounded mt-4"
+        >
+          Generate image
+        </button>
+            )
+        }
       </div>
-    )}
-  </div>
- );
+    </div>
+  );
 }
