78382af477ed6743bb2e03f5466e87c3568b28f0
deveshb-am <devesh.b@asset.money>
2023-11-22 15:51:31 +0530

    added more api keys

--- constants/keys.js
+++ constants/keys.js
@@ -18,5 +18,25 @@ export const keys = [
     {
         userid: "b4fbc5a4-1560-4db0-af16-90583269dda5",
         apikey: "b771dc57-f548-4dce-b817-836c5040e1d1"
+    },
+    {
+        userid: "7e24f9d1-9e1a-4fe9-bd1f-bbd01835bb46",
+        apikey: "332aba61-6fc7-4ab5-9043-88842baa9bb3"
+    },
+    {
+        userid: "f5a35ce1-ab91-4d0f-965b-05c6639cf623",
+        apikey: "f21775c8-e9ef-4ace-b821-67b6ae6c692d"
+    },
+    {
+        userid: "b1995384-6887-4a30-ad97-532dfc46955d",
+        apikey: "20829a17-1f90-4f93-a9ad-0475820f3b9d"
+    },
+    {
+        userid: "ba40d257-5cf4-4ff9-9432-ae811d3cb821",
+        apikey: "7e45e8f2-ce5a-4ed6-8cb0-b727521f59df"
+    },
+    {
+        userid: "102a0d3a-ab66-475e-a7e1-d2ebe9d80d1e",
+        apikey: "6ca24808-221a-47c9-bd06-f20b413db534"
     }
 ]
